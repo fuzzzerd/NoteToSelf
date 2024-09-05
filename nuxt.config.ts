8e27ce5968b15a48f9507133822650a4fb678044
Nate Bross <nate@brosstribe.com>
2024-09-04 20:01:55 -0500

    clean up some deps and builds

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,7 @@ export default defineNuxtConfig({
   site: {
     url: 'https://www.brosstribe.com'
   },
-  modules: [
-    'nuxt-content-assets',
-    '@nuxt/content',
-    '@nuxt/image',
-    '@nuxtjs/seo'
-  ],
+  modules: ['nuxt-content-assets', '@nuxt/content', '@nuxtjs/seo'],
   content: {
     documentDriven: false,
     highlight: {
@@ -31,16 +26,8 @@ export default defineNuxtConfig({
   sitemap: {
     sources: ['/api/sitemap']
   },
-  image: {
-    dir: '.nuxt/content-assets/public'
-  },
   experimental: {
     sharedPrerenderData: true
   },
-  // fix for https://github.com/davestewart/nuxt-content-assets/issues/49
-  hooks: {
-    close: (nuxt) => {
-      if (!nuxt.options._prepare) process.exit();
-    }
-  }
+  compatibilityDate: '2024-09-04'
 });
